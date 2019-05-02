33e7fbd2c39399729d07a8521cbd57bb558a5837
yaishenka <gagarinovdaniil@gmail.com>
2019-05-02 15:11:23 +0000

    fighting with vk verify

--- vk_sender/views.py
+++ vk_sender/views.py
@@ -1,14 +1,13 @@
 from django.shortcuts import render, redirect, HttpResponse
 from .tasks import send_vk_message
 from django.views.decorators.csrf import csrf_exempt
-
+import json
 
 
 # Create your views here.
 @csrf_exempt
 def vk_callback(request):
-    request_type = request.POST['type']
+    request_type = json.loads(request.body.decode('utf-8')['type']
     if request_type == "confirmation":
         return HttpResponse("07ca78fc", content_type="text/plain", status=200)
-
     return redirect('ads_list')
