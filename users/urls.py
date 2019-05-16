926d48352639cefabdcf9a35ee353f6c4c8d361a
yaishenka <gagarinovdaniil@gmail.com>
2019-05-16 18:14:19 +0300

    remove registration from site

--- users/urls.py
+++ users/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
-    path('register/', views.user_register, name='register'),
+    # path('register/', views.user_register, name='register'),
     path('<int:user_id>', views.user_view, name='user_view'),
     path('private_office', views.private_office, name='private_office')
     #TODO ChangePassword, ForgotPassword
